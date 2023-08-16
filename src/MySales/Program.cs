3017868fd864184f8e69fae79b2271b0a48c2afd
Tamiriam Fernandes <tamiriam.fernandes@hotmail.com>
2023-08-15 21:42:52 -0300

    ajustado para abrir swagger

--- src/MySales/Program.cs
+++ src/MySales/Program.cs
@@ -13,7 +13,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+    });
 }
 
 app.UseHttpsRedirection();
