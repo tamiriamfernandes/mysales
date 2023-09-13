467d43ff3a0c444cd1af4635ab1a98876614cca1
Tamiriam Fernandes <tamiriam.fernandes@hotmail.com>
2023-09-13 06:46:32 -0300

    Criado projeto de test unitario e implementado teste de criação do customer

---
+++ src/MySales.Tests/CustomerCrudUseCaseTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Moq;
+using MySales.Application.Contracts.Repositories;
+using MySales.Application.UseCases;
+using MySales.Model.DTOs.Client;
+using MySales.Model.Entities;
+using Xunit;
+
+namespace MySales.UseCase.Tests;
+
+public class CustomerCrudUseCaseTests
+{
+    private readonly Mock<IRepository<Customer>> mockCustomerRepository = new Mock<IRepository<Customer>>();
+    private readonly Mock<IMapper> mockMapper = new Mock<IMapper>();
+
+    [Fact]
+    public async Task CreateAsync_NewCustomer_ReturnCustomerId()
+    {
+        // Arrange
+        var createClientDto = new CreateCustomerDto("Tamiriam", "17996521880");
+
+        var expectedClientId = 1;
+
+        mockMapper.Setup(mapper => mapper.Map<Customer>(It.IsAny<CreateCustomerDto>()))
+                 .Returns(new Customer());
+
+        mockCustomerRepository.Setup(repo => repo.Add(It.IsAny<Customer>()))
+                            .Callback<Customer>(customer => customer.Id = expectedClientId);
+
+        var clientService = new CustomerCrudUseCase(mockMapper.Object, mockCustomerRepository.Object);
+
+        // Act
+        var result = await clientService.CreateAsync(createClientDto);
+
+        // Assert
+        Assert.Equal(expectedClientId, result);
+
+        mockCustomerRepository.Verify(repo => repo.SaveChangesAsync(), Times.Once);
+    }
+}
