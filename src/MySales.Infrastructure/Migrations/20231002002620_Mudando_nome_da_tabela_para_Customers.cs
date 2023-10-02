7e5a30217410c0efc6fc852edacd226ffee3c25a
Tamiriam Fernandes <tamiriam.fernandes@hotmail.com>
2023-10-01 21:27:31 -0300

    Criado migration para mudar tabela client para customer

---
+++ src/MySales.Infrastructure/Migrations/20231002002620_Mudando_nome_da_tabela_para_Customers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MySales.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mudando_nome_da_tabela_para_Customers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(11)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    Name = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(11)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+    }
+}
